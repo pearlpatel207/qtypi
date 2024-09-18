ab9e3098c843a453e829776f3a370edb26b97aec
unknown <pearl207@gmail.com>
2024-09-18 15:04:06 -0700

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='qtypi',
+    name='qtypi',  # Make sure the package name is unique and valid
     version='0.1.0',
     description='A Python library for quantum state manipulations and quantum gates',
-    author='Your Name',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',  # Include the content type
+    author='Pearl',
+    url='https://github.com/pearlpatel207/qtypi',  # Your project's URL
     packages=find_packages(),
     install_requires=['numpy'],
     classifiers=[
